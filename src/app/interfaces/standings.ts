ab4ec6fb17c3156f69fb01b1340a3db7736a831b
axg2007 <ankit.gupta@iff.com>
2023-09-22 17:21:14 +0530

    Add type

--- src/app/interfaces/standings.ts
+++ src/app/interfaces/standings.ts
@@ -1,11 +1,27 @@
+export interface Team {
+  id: number;
+  logo: string;
+  name: string;
+}
+
+export interface ALL {
+  played: string;
+  win: string;
+  draw: string;
+  lose: String;
+}
+
 export interface Standings {
-    name: string,
-    logo: string,
-    noOfGames: number,
-    wins: number,
-    losses: number,
-    draws: number,
-    goalDifference: number,
-    points: number
-  }
-  
+  name: string;
+  logo: string;
+  noOfGames: number;
+  wins: number;
+  losses: number;
+  draws: number;
+  goalDifference: number;
+  points: number;
+  rank: number;
+  team: Team;
+  all: ALL;
+  goalsDiff: number;
+}
